52093af3180afaee0cb189b5d37c43e575112d4a
Mariohmis <mariohmis@gmail.com,noreply.github.com>
2024-01-24 22:06:33 -0600

    worrking in the colors and buttons

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+let color = 'Black';
+
 document.addEventListener('DOMContentLoaded', function () {
-  createBoard(64);
-  console.log('Hi');
+  createBoard();
+
+  let btn_popup = document.querySelector('#popup');
+  btn_popup.addEventListener('click', function () {
+    let size = getSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
@@ -9,10 +16,43 @@ function createBoard(size) {
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  let numGrids = size * size;
+  let numDivs = size * size;
 
-  for (let i = 0; i < numGrids; i++) {
-    let grid = document.createElement('div');
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement('div');
+    div.addEventListener('mouseover', colorDiv);
+
     board.insertAdjacentElement('beforeend', div);
   }
+}
+
+function getSize() {
+  let input = prompt('Size of the board');
+  let message = document.querySelector('#message');
+
+  if (input == '') {
+    message.innerHTML = 'Please provide a number';
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = 'Provide a number bettwen 1 and 100';
+  } else {
+    message.innerHTML = 'Now you can play!';
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == 'random') {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = 'black';
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function clearBoard() {
+  let divs = document.querySelectorAll('div');
+  divs.forEach((div) => (div.style.backgroundColor = 'white'));
 }
